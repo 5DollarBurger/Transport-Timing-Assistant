a1f3957955facca555882737a9a9e7bcc2212f04
Darrell Leong <darrelllwk@gmail.com>
2022-11-19 22:16:37 +0800

    receive inputs

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from "react-native";
+import SelectDropdown from "react-native-select-dropdown";
+
+const destinations = ["Tsing Yi", "Kwai Fong"];
 
 export default function App() {
+  function destinationHandler(selectedDestination) {
+    // console.log(selectedDestination);
+    if (selectedDestination == "Tsing Yi") {
+      console.log("Bus 330");
+    } else {
+      console.log("Bus 332");
+    }
+  }
+
+  function getVehicle() {}
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View>
+        {/* <TextInput placeholder="Your destination"></TextInput>
+        <Button title="Enter" /> */}
+        <SelectDropdown data={destinations} onSelect={destinationHandler} />
+      </View>
+      <View>
+        <Text>Results...</Text>
+      </View>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50,
   },
 });
