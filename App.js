649eeb41393a2713a474b8bc03cbed5eadc2976b
Darrell Leong <darrelllwk@gmail.com>
2022-11-24 14:35:39 +0800

    got read slqite db to work

--- App.js
+++ App.js
@@ -1,8 +1,47 @@
 // import { StatusBar } from 'expo-status-bar';
+import { Asset } from "expo-asset";
+import * as FileSystem from "expo-file-system";
+import * as SQLite from "expo-sqlite";
+// import { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import SelectDropdown from "react-native-select-dropdown";
 
 const destinations = ["Tsing Yi", "Kwai Fong"];
+
+// DB ACCESS START
+// const db = SQLite.openDatabase("departures.db");
+const openDatabase = async () => {
+  if (
+    (await FileSystem.getInfoAsync(FileSystem.documentDirectory + "SQLite"))
+      .exists
+  ) {
+    await FileSystem.downloadAsync(
+      Asset.fromModule(require("./assets/departures.db")).uri,
+      FileSystem.documentDirectory + "SQLite/departures.db"
+    );
+    const db = await SQLite.openDatabase("departures.db");
+    return db;
+  }
+  return alert("SQLite database not found");
+};
+// const db = openDatabase();
+
+const getData = () => {
+  // console.log(db);
+  openDatabase().then((db) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "SELECT name FROM dim_destination",
+        [],
+        (trans, result) => {
+          console.log(result.rows._array);
+        },
+        (trans, error) => console.log("Error ", error)
+      );
+    });
+  });
+};
+// DB ACCESS END
 
 export default function App() {
   function destinationHandler(selectedDestination) {
@@ -12,6 +51,21 @@ export default function App() {
     } else {
       console.log("Bus 332");
     }
+    console.log(getData());
+
+    // fetchData = () => {
+    //   db.transaction((tx) => {
+    //     // sending 4 arguments in executeSql
+    //     tx.executeSql(
+    //       "SELECT name FROM dim_destination",
+    //       null, // passing sql query and parameters:null
+    //       // success callback which sends two things Transaction object and ResultSet Object
+    //       (txObj, { rows: { _array } }) => this.setState(({ data: _array }),
+    //       // failure callback which sends two things Transaction object and Error
+    //       (txObj, error) => console.log("Error ", error)
+    //     ); // end executeSQL
+    //   }); // end transaction
+    // };
   }
 
   function getVehicle() {}
--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # Transport-Timing-Assistant
 
 This app provides decision support on when to leave your home, so that you can board your public transport with minimal waiting.
+
+## Setup
+
+- Navigate to node_modules/react-native/react-native.config.js
+- Add the following:
+
+```
+module.exports = {
+  ...,
+  dependencies: {
+    ...,
+    "react-native-sqlite-storage": {
+      platforms: {
+        android: {
+          sourceDir:
+            "../node_modules/react-native-sqlite-storage/platforms/android-native",
+          packageImportPath: "import io.liteglue.SQLitePluginPackage;",
+          packageInstance: "new SQLitePluginPackage()"
+        }
+      }
+    }
+    ...
+  }
+  ...
+};
+```
